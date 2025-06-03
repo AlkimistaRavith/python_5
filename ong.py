340ef7f2d721e338f85eb513c5766060d70ccfe1
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-03 05:08:07 +0000

    avance

--- ong.py
+++ ong.py
@@ -14,6 +14,19 @@ def productoria(lista):
         prod_res *= numero
     return prod_res
 
-def calcular()
+def calcular(**kwargs):
+    for clave, valor in kwargs.items():
+        if clave.startswith('fact_'):
+            if isinstance(valor, int): 
+                print(f"El factorial de {valor} = {factorial(valor)}")
+            else:
+                print(f"Valor no válido para el factorial {clave}. Debe ser un número entero (n).")
+        elif clave.startswith('prod_'):
+            if isinstance(valor, list):
+                print(f"La productoria de {valor} = {productoria(valor)}")
+            else:
+                print(f"Valor no válido para la productoria {clave}. Debe ser una lista de números entero ([n1,n2,n3,...,etc.]).")
+        else:
+            print(f"El argumento {clave}, no cumple con el formato. Por favor utiliza fact_i o prod_i para calcular un factorial o una productoria, respectivamente.")
 
-print(productoria([2,4,5,7]))
+calcular(fact_1=[1,2,3], prod_1=[3,6,4,2,8], fact_2=6)clear
