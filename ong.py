1529bc3029d51eb15b47d4cbf1a0e7e0b307120e
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-03 03:53:46 +0000

    avance ejercicio3

---
+++ ong.py
@@ -0,0 +1,19 @@
+
+
+
+
+def factorial(n):
+    fact_res = 1
+    for i in range(2, n + 1):
+        fact_res *= i
+    return fact_res
+
+def productoria(lista):
+    prod_res = 1
+    for numero in lista:
+        prod_res *= numero
+    return prod_res
+
+def calcular()
+
+print(productoria([2,4,5,7]))
