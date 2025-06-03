1930a3198b89992f830869e14a50bef9f0fcb354
AlkimistaRavith <ravith.d3@gmail.com>
2025-06-03 05:12:33 +0000

    Trabajo terminado

--- ong.py
+++ ong.py
@@ -1,19 +1,19 @@
-
-
-
-
+#Calculo de factorial
 def factorial(n):
+    #fact_res = resultado de factoria
     fact_res = 1
     for i in range(2, n + 1):
         fact_res *= i
     return fact_res
-
+#Calculo de productoria
 def productoria(lista):
+    #prod_res = resultado de productoria
     prod_res = 1
     for numero in lista:
         prod_res *= numero
     return prod_res
 
+#función que llama un calculo u otro, según llamada. Para valores que no corresponde, entrega mensaje de error.
 def calcular(**kwargs):
     for clave, valor in kwargs.items():
         if clave.startswith('fact_'):
@@ -27,6 +27,6 @@ def calcular(**kwargs):
             else:
                 print(f"Valor no válido para la productoria {clave}. Debe ser una lista de números entero ([n1,n2,n3,...,etc.]).")
         else:
-            print(f"El argumento {clave}, no cumple con el formato. Por favor utiliza fact_i o prod_i para calcular un factorial o una productoria, respectivamente.")
+            print(f"El argumento {clave}, no cumple con el formato. \n    Por favor utiliza fact_i=n o prod_i=[n1,n2,n3,...,etc.] para calcular un factorial o una productoria, respectivamente.")
 
-calcular(fact_1=5, prod_1=[3,6,4,2,8], fact_2=6)
+calcular(fct_1=5, prod_1=[3,6,4,2,8], fact_2=6)
